3ab17024b24a9c40f2f2f1f0755dabe9f91ef59c
LLambert <Leon.Lambert@honeywell.com>
2019-02-28 08:13:33 -0600

    convert to maven project but retain eclipse project

---
+++ src/test/java/per/lambert/ebattleMat/client/ElectronicBattleMatTest.java
@@ -0,0 +1,30 @@
+package per.lambert.ebattleMat.client;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.junit.client.GWTTestCase;
+
+/**
+ * GWT JUnit tests must extend GWTTestCase.
+ */
+public class ElectronicBattleMatTest extends GWTTestCase {
+
+	/**
+	 * Must refer to a valid module that sources this class.
+	 */
+	public String getModuleName() {
+		return "per.lambert.ebattleMat.ElectronicBattleMatJUnit";
+	}
+
+	/**
+	 * Tests the FieldVerifier.
+	 */
+	public void testFieldVerifier() {
+	}
+
+	/**
+	 * This test will send a request to the server using the greetServer method in
+	 * GreetingService and verify the response.
+	 */
+	public void testGreetingService() {
+	}
+}
