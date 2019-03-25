cd3ec58da4aa21eef8389e661ab76489d0fe2175
LLambert <Leon.Lambert@honeywell.com>
2019-03-25 04:06:30 -0500

    try adding unit tests

---
+++ src/test/java/per/lambert/ebattleMat/client/ElectronicBattleMatTest.java
@@ -0,0 +1,26 @@
+package per.lambert.ebattleMat.client;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+/**
+ * GWT JUnit tests must extend GWTTestCase.
+ */
+public class ElectronicBattleMatTest extends GWTTestCase {
+
+	/**
+	 * Must refer to a valid module that sources this class.
+	 */
+	public String getModuleName() {
+		return "per.lambert.ebattleMat.ElectronicBattleMatJUnit";
+	}
+
+
+	/**
+	 * This test will send a request to the server using the greetServer method in
+	 * GreetingService and verify the response.
+	 */
+	public void testGreetingService() {
+		assertTrue(true);
+	}
+
+}
