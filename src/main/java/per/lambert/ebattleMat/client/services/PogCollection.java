5957bb9526c41e32a74d169c41ccdacaa0f15f30
LLambert <LambertLB@aol.com>
2023-02-13 09:19:40 -0600

    refactored pog manager using pog collection

---
+++ src/main/java/per/lambert/ebattleMat/client/services/PogCollection.java
@@ -0,0 +1,234 @@
+/*
+ * Copyright (C) 2019 Leon Lambert.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package per.lambert.ebattleMat.client.services;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.gwt.core.client.JsonUtils;
+
+import per.lambert.ebattleMat.client.event.ReasonForActionEvent;
+import per.lambert.ebattleMat.client.interfaces.IDataRequester;
+import per.lambert.ebattleMat.client.interfaces.IErrorInformation;
+import per.lambert.ebattleMat.client.interfaces.IUserCallback;
+import per.lambert.ebattleMat.client.interfaces.ReasonForAction;
+import per.lambert.ebattleMat.client.services.serviceData.PogData;
+import per.lambert.ebattleMat.client.services.serviceData.PogList;
+
+/**
+ * Pog collection.
+ * 
+ * This will hold information about a pog list.
+ * 
+ * @author llambert
+ *
+ */
+public class PogCollection {
+	/**
+	 * load event.
+	 */
+	private ReasonForAction loadEvent;
+	/**
+	 * Map of pog data vs name.
+	 */
+	private Map<String, PogData> pogTemplateMap = new HashMap<String, PogData>();
+	/**
+	 * Map of pog races.
+	 */
+	private Map<String, String> pogRaces = new HashMap<String, String>();
+	/**
+	 * Map of pog classes.
+	 */
+	private Map<String, String> pogClasses = new HashMap<String, String>();
+	/**
+	 * List of pog templates.
+	 */
+	private PogList pogTemplates;
+
+	/**
+	 * get array of pog templates.
+	 * 
+	 * @return array of pog templates
+	 */
+	public PogData[] getPogList() {
+		return pogTemplates.getPogList();
+	}
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param eventToFireWhenLoaded event to fire when loaded
+	 */
+	public PogCollection(final ReasonForAction eventToFireWhenLoaded) {
+		loadEvent = eventToFireWhenLoaded;
+	}
+
+	/**
+	 * Find this pog based on UUID.
+	 * 
+	 * @param pogUUID to find
+	 * @return pog data if found
+	 */
+	public PogData findPog(final String pogUUID) {
+		return (pogTemplateMap.get(pogUUID));
+	}
+
+	/**
+	 * Clear out collections.
+	 */
+	public void clear() {
+		pogTemplateMap.clear();
+		pogRaces.clear();
+		pogClasses.clear();
+	}
+
+	/**
+	 * Load in Pogs from resource file.
+	 * 
+	 * @param typeToLoad type to load
+	 */
+	public void loadFromServer(final String typeToLoad) {
+		clear();
+		IDataRequester dataRequester = ServiceManager.getDataRequester();
+		Map<String, String> parameters = new HashMap<String, String>();
+		parameters.put("fileName", typeToLoad + "pogs.json");
+		dataRequester.requestData("", "LOADJSONFILE", parameters, new IUserCallback() {
+
+			@Override
+			public void onSuccess(final Object sender, final Object data) {
+				loadPogTemplates(data);
+			}
+
+			@Override
+			public void onError(final Object sender, final IErrorInformation error) {
+			}
+		});
+	}
+
+	/**
+	 * Got data from server fill in collections.
+	 * 
+	 * @param data received
+	 */
+	private void loadPogTemplates(final Object data) {
+		pogTemplates = JsonUtils.<PogList>safeEval((String) data);
+		rebuildCollections();
+	}
+
+	/**
+	 * Rebuild all collections.
+	 */
+	private void rebuildCollections() {
+		clear();
+		for (PogData pogTemplate : pogTemplates.getPogList()) {
+			addToCollections(pogTemplate);
+		}
+		ServiceManager.getEventManager().fireEvent(new ReasonForActionEvent(loadEvent, null));
+	}
+
+	/**
+	 * Add information to various collections.
+	 * 
+	 * @param pogToAdd with data to add
+	 */
+	private void addToCollections(final PogData pogToAdd) {
+		if (!pogToAdd.getPogClass().isEmpty()) {
+			if (!pogClasses.containsKey(pogToAdd.getPogClass())) {
+				pogClasses.put(pogToAdd.getPogClass(), pogToAdd.getPogClass());
+			}
+		}
+		if (!pogToAdd.getRace().isEmpty()) {
+			if (!pogRaces.containsKey(pogToAdd.getRace())) {
+				pogRaces.put(pogToAdd.getRace(), pogToAdd.getRace());
+			}
+		}
+		pogTemplateMap.put(pogToAdd.getUUID(), pogToAdd);
+	}
+
+	/**
+	 * add or update this pog.
+	 * 
+	 * @param pog to add or update
+	 */
+	public void addOrUpdatePogToCommonResource(final PogData pog) {
+		if (findPog(pog.getUUID()) == null) {
+			pogTemplates.addPog(pog);
+		}
+		rebuildCollections();
+	}
+
+	/**
+	 * Filter a pog list.
+	 * 
+	 * @param raceFilter race filter
+	 * @param classFilter class filter
+	 * @param genderFilter gender filter
+	 * @return pog that match
+	 */
+	public ArrayList<PogData> getFilteredPogs(final String raceFilter, final String classFilter, final String genderFilter) {
+		ArrayList<PogData> filteredPogs = new ArrayList<PogData>();
+		for (PogData pog : pogTemplates.getPogList()) {
+			if (raceFilter != null && !raceFilter.isEmpty()) {
+				if (!pog.getRace().equalsIgnoreCase(raceFilter)) {
+					continue;
+				}
+			}
+			if (classFilter != null && !classFilter.isEmpty()) {
+				if (!pog.getPogClass().equalsIgnoreCase(classFilter)) {
+					continue;
+				}
+			}
+			if (genderFilter != null && !genderFilter.isEmpty()) {
+				if (!pog.getGender().equalsIgnoreCase(genderFilter)) {
+					continue;
+				}
+			}
+			filteredPogs.add(pog);
+		}
+		return (filteredPogs);
+	}
+
+	/**
+	 * get Array of classes.
+	 * 
+	 * @return array of classes.
+	 */
+	public String[] getClassses() {
+		String[] rtn = new String[0];
+		return (pogClasses.values().toArray(rtn));
+	}
+
+	/**
+	 * get Array of races.
+	 * 
+	 * @return array of races.
+	 */
+	public String[] getRaces() {
+		String[] rtn = new String[0];
+		return (pogRaces.values().toArray(rtn));
+	}
+
+	/**
+	 * Is this pog a template.
+	 * 
+	 * @param pogData to check
+	 * @return true if is template
+	 */
+	public boolean isTemplate(final PogData pogData) {
+		return (pogTemplateMap.containsKey(pogData.getUUID()));
+	}
+}
--- src/main/java/per/lambert/ebattleMat/client/services/PogManager.java
+++ src/main/java/per/lambert/ebattleMat/client/services/PogManager.java
@@ -17,24 +17,17 @@ package per.lambert.ebattleMat.client.services;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 
 import com.google.gwt.core.client.JavaScriptObject;
-import com.google.gwt.core.client.JsonUtils;
 
 import per.lambert.ebattleMat.client.event.ReasonForActionEvent;
 import per.lambert.ebattleMat.client.interfaces.Constants;
 import per.lambert.ebattleMat.client.interfaces.DungeonMasterFlag;
 import per.lambert.ebattleMat.client.interfaces.Gender;
-import per.lambert.ebattleMat.client.interfaces.IDataRequester;
-import per.lambert.ebattleMat.client.interfaces.IErrorInformation;
 import per.lambert.ebattleMat.client.interfaces.IPogManager;
-import per.lambert.ebattleMat.client.interfaces.IUserCallback;
 import per.lambert.ebattleMat.client.interfaces.PlayerFlag;
 import per.lambert.ebattleMat.client.interfaces.ReasonForAction;
 import per.lambert.ebattleMat.client.services.serviceData.PogData;
-import per.lambert.ebattleMat.client.services.serviceData.PogList;
 
 /**
  * Pog manager.
@@ -44,55 +37,36 @@ import per.lambert.ebattleMat.client.services.serviceData.PogList;
  */
 public abstract class PogManager implements IPogManager {
 	/**
-	 * Map of monster pogs.
+	 * Collection of monster templates.
 	 */
-	private Map<String, PogData> monsterTemplateMap = new HashMap<String, PogData>();
+	private PogCollection monsterCollection = new PogCollection(ReasonForAction.MonsterPogsLoaded);
+
 	/**
-	 * Map of monster races.
+	 * Collection of room templates.
 	 */
-	private Map<String, String> monsterRaces = new HashMap<String, String>();
-	/**
-	 * Map of monster classes.
-	 */
-	private Map<String, String> monsterClasses = new HashMap<String, String>();
-	/**
-	 * List of monster pogs.
-	 */
-	private PogList monsterTemplatePogs;
+	private PogCollection roomCollection = new PogCollection(ReasonForAction.RoomObjectPogsLoaded);
+
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
 	public PogData[] getMonsterTemplatePogs() {
-		return monsterTemplatePogs.getPogList();
+		return monsterCollection.getPogList();
 	}
-	/**
-	 * Map of room object pogs.
-	 */
-	private Map<String, PogData> roomObjectTemplateMap = new HashMap<String, PogData>();
-	/**
-	 * Map of room object races.
-	 */
-	private Map<String, String> roomObjectRaces = new HashMap<String, String>();
-	/**
-	 * Map of room object classes.
-	 */
-	private Map<String, String> roomObjectClasses = new HashMap<String, String>();
-	/**
-	 * List of room objects.
-	 */
-	private PogList roomObjectTemplatePogs;
+
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
 	public PogData[] getRoomObjectTemplatePogs() {
-		return roomObjectTemplatePogs.getPogList();
+		return roomCollection.getPogList();
 	}
+
 	/**
 	 * Currently selected Pog.
 	 */
 	private PogData selectedPog;
+
 	/**
 	 * {@inheritDoc}
 	 */
@@ -100,6 +74,7 @@ public abstract class PogManager implements IPogManager {
 	public PogData getSelectedPog() {
 		return selectedPog;
 	}
+
 	/**
 	 * {@inheritDoc}
 	 */
@@ -117,6 +92,7 @@ public abstract class PogManager implements IPogManager {
 	protected void setSelectedPogInternal(final PogData selectedPog) {
 		this.selectedPog = selectedPog;
 	}
+
 	/**
 	 * Set selected pog.
 	 * 
@@ -189,7 +165,7 @@ public abstract class PogManager implements IPogManager {
 	 */
 	@Override
 	public PogData findMonsterPog(final String pogUUID) {
-		return (monsterTemplateMap.get(pogUUID));
+		return (monsterCollection.findPog(pogUUID));
 	}
 
 	/**
@@ -197,7 +173,7 @@ public abstract class PogManager implements IPogManager {
 	 */
 	@Override
 	public PogData findRoomObjectPog(final String pogUUID) {
-		return (roomObjectTemplateMap.get(pogUUID));
+		return (roomCollection.findPog(pogUUID));
 	}
 
 	/**
@@ -224,121 +200,14 @@ public abstract class PogManager implements IPogManager {
 	 * Load in monster Pogs from resource file.
 	 */
 	public void loadMonsterPogs() {
-		monsterTemplatePogs = null;
-		IDataRequester dataRequester = ServiceManager.getDataRequester();
-		Map<String, String> parameters = new HashMap<String, String>();
-		parameters.put("fileName", Constants.DUNGEON_MONSTER_LOCATION + "pogs.json");
-		dataRequester.requestData("", "LOADJSONFILE", parameters, new IUserCallback() {
-
-			@Override
-			public void onSuccess(final Object sender, final Object data) {
-				loadMonsterPogTemplates(data);
-			}
-
-			@Override
-			public void onError(final Object sender, final IErrorInformation error) {
-			}
-		});
-	}
-
-	/**
-	 * Load in monster Pog templates from data.
-	 * 
-	 * @param data with Pogs.
-	 */
-	protected void loadMonsterPogTemplates(final Object data) {
-		monsterTemplateMap.clear();
-		monsterClasses.clear();
-		monsterRaces.clear();
-		monsterTemplatePogs = JsonUtils.<PogList>safeEval((String) data);
-		rebuildMonsterCollections();
-	}
-
-	/**
-	 * Rebuild collections for filters.
-	 */
-	private void rebuildMonsterCollections() {
-		monsterClasses.clear();
-		monsterRaces.clear();
-		for (PogData monsterTemplate : monsterTemplatePogs.getPogList()) {
-			addMonsterToCollections(monsterTemplate);
-		}
-		ServiceManager.getEventManager().fireEvent(new ReasonForActionEvent(ReasonForAction.MonsterPogsLoaded, null));
-	}
-
-	/**
-	 * Add Monster pog to collections.
-	 * 
-	 * @param monsterTemplate to add.
-	 */
-	private void addMonsterToCollections(final PogData monsterTemplate) {
-		if (!monsterTemplate.getPogClass().isEmpty()) {
-			if (!monsterClasses.containsKey(monsterTemplate.getPogClass())) {
-				monsterClasses.put(monsterTemplate.getPogClass(), monsterTemplate.getPogClass());
-			}
-		}
-		if (!monsterTemplate.getRace().isEmpty()) {
-			if (!monsterRaces.containsKey(monsterTemplate.getRace())) {
-				monsterRaces.put(monsterTemplate.getRace(), monsterTemplate.getRace());
-			}
-		}
-		monsterTemplateMap.put(monsterTemplate.getUUID(), monsterTemplate);
+		monsterCollection.loadFromServer(Constants.DUNGEON_MONSTER_LOCATION);
 	}
 
 	/**
 	 * Load in room object Pogs from resource file.
 	 */
 	public void loadRoomObjectPogs() {
-		roomObjectTemplatePogs = null;
-		IDataRequester dataRequester = ServiceManager.getDataRequester();
-		Map<String, String> parameters = new HashMap<String, String>();
-		parameters.put("fileName", Constants.DUNGEON_ROOMOBJECT_LOCATION + "pogs.json");
-		dataRequester.requestData("", "LOADJSONFILE", parameters, new IUserCallback() {
-
-			@Override
-			public void onSuccess(final Object sender, final Object data) {
-				loadRoomPogTemplates(data);
-			}
-
-			@Override
-			public void onError(final Object sender, final IErrorInformation error) {
-			}
-		});
-	}
-
-	/**
-	 * Load in room object Pog templates from data.
-	 * 
-	 * @param data with Pogs.
-	 */
-	protected void loadRoomPogTemplates(final Object data) {
-		roomObjectTemplateMap.clear();
-		roomObjectClasses.clear();
-		roomObjectRaces.clear();
-		roomObjectTemplatePogs = JsonUtils.<PogList>safeEval((String) data);
-		for (PogData roomObjectTemplate : roomObjectTemplatePogs.getPogList()) {
-			addRoomObjectToCollections(roomObjectTemplate);
-		}
-		ServiceManager.getEventManager().fireEvent(new ReasonForActionEvent(ReasonForAction.RoomObjectPogsLoaded, null));
-	}
-
-	/**
-	 * Add Room Object pog to collections.
-	 * 
-	 * @param roomObjectTemplate to add.
-	 */
-	private void addRoomObjectToCollections(final PogData roomObjectTemplate) {
-		if (!roomObjectTemplate.getPogClass().isEmpty()) {
-			if (!roomObjectClasses.containsKey(roomObjectTemplate.getPogClass())) {
-				roomObjectClasses.put(roomObjectTemplate.getPogClass(), roomObjectTemplate.getPogClass());
-			}
-		}
-		if (!roomObjectTemplate.getRace().isEmpty()) {
-			if (!roomObjectRaces.containsKey(roomObjectTemplate.getRace())) {
-				roomObjectRaces.put(roomObjectTemplate.getRace(), roomObjectTemplate.getRace());
-			}
-		}
-		roomObjectTemplateMap.put(roomObjectTemplate.getUUID(), roomObjectTemplate);
+		roomCollection.loadFromServer(Constants.DUNGEON_ROOMOBJECT_LOCATION);
 	}
 
 	/**
@@ -346,40 +215,10 @@ public abstract class PogManager implements IPogManager {
 	 */
 	@Override
 	public ArrayList<PogData> getFilteredCommonTemplates(final String pogType, final String raceFilter, final String classFilter, final String genderFilter) {
-		PogList pogsToSearch = pogType.equals(Constants.POG_TYPE_MONSTER) ? monsterTemplatePogs : roomObjectTemplatePogs;
-		return (getFilteredPogs(pogsToSearch, raceFilter, classFilter, genderFilter));
-	}
-
-	/**
-	 * Filter a pog list.
-	 * 
-	 * @param pogsToSearch pog list to search
-	 * @param raceFilter race filter
-	 * @param classFilter class filter
-	 * @param genderFilter gender filter
-	 * @return pog that match
-	 */
-	private ArrayList<PogData> getFilteredPogs(final PogList pogsToSearch, final String raceFilter, final String classFilter, final String genderFilter) {
-		ArrayList<PogData> filteredPogs = new ArrayList<PogData>();
-		for (PogData pog : pogsToSearch.getPogList()) {
-			if (raceFilter != null && !raceFilter.isEmpty()) {
-				if (!pog.getRace().equalsIgnoreCase(raceFilter)) {
-					continue;
-				}
-			}
-			if (classFilter != null && !classFilter.isEmpty()) {
-				if (!pog.getPogClass().equalsIgnoreCase(classFilter)) {
-					continue;
-				}
-			}
-			if (genderFilter != null && !genderFilter.isEmpty()) {
-				if (!pog.getGender().equalsIgnoreCase(genderFilter)) {
-					continue;
-				}
-			}
-			filteredPogs.add(pog);
+		if (pogType.equals(Constants.POG_TYPE_MONSTER)) {
+			return (monsterCollection.getFilteredPogs(raceFilter, classFilter, genderFilter));
 		}
-		return (filteredPogs);
+		return (roomCollection.getFilteredPogs(raceFilter, classFilter, genderFilter));
 	}
 
 	/**
@@ -387,12 +226,10 @@ public abstract class PogManager implements IPogManager {
 	 */
 	@Override
 	public String[] getCommonClasses(final String pogType) {
-		String[] rtn = new String[0];
 		if (pogType.equals(Constants.POG_TYPE_MONSTER)) {
-			return (monsterClasses.values().toArray(rtn));
+			return (monsterCollection.getClassses());
 		}
-		return (roomObjectClasses.values().toArray(rtn));
-
+		return (roomCollection.getClassses());
 	}
 
 	/**
@@ -400,12 +237,10 @@ public abstract class PogManager implements IPogManager {
 	 */
 	@Override
 	public String[] getCommonRaces(final String pogType) {
-		String[] rtn = new String[0];
 		if (pogType.equals(Constants.POG_TYPE_MONSTER)) {
-			return (monsterRaces.values().toArray(rtn));
+			return (monsterCollection.getRaces());
 		}
-		return (roomObjectRaces.values().toArray(rtn));
-
+		return (roomCollection.getClassses());
 	}
 
 	/**
@@ -433,15 +268,10 @@ public abstract class PogManager implements IPogManager {
 	 */
 	protected void addOrUpdatePogToCommonResource(final PogData pog) {
 		if (pog.isThisAMonster()) {
-			if (findMonsterPog(pog.getUUID()) == null) {
-				monsterTemplatePogs.addPog(pog);
-			}
-			return;
+			monsterCollection.addOrUpdatePogToCommonResource(pog);
+		} else {
+			roomCollection.addOrUpdatePogToCommonResource(pog);
 		}
-		if (findRoomObjectPog(pog.getUUID()) == null) {
-			roomObjectTemplatePogs.addPog(pog);
-		}
-		rebuildMonsterCollections();
 	}
 
 	/**
@@ -453,9 +283,9 @@ public abstract class PogManager implements IPogManager {
 	@Override
 	public boolean isTemplate(final PogData pogData) {
 		if (pogData.isThisAMonster()) {
-			return (monsterTemplateMap.containsKey(pogData.getUUID()));
+			return (monsterCollection.isTemplate(pogData));
 		} else if (pogData.isThisARoomObject()) {
-			return (roomObjectTemplateMap.containsKey(pogData.getUUID()));
+			return (roomCollection.isTemplate(pogData));
 		}
 		return (false);
 	}
